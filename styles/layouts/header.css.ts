3b74d810f6910073dbfa92c93b93ce29ad598f48
NahuriraClintonie <clintonnahurira24@gmail.com>
2025-06-11 16:59:21 +0300

    fixed the display of the items in the header

--- styles/layouts/header.css.ts
+++ styles/layouts/header.css.ts
@@ -6,14 +6,15 @@ export const headerRoot = style({
   top: 0,
   left: 0,
   right: 0,
-  zIndex: 1000, // Ensure it stays above everything
-  // backgroundColor: vars.color.white,
-  padding: `${vars.spacing.medium} ${vars.spacing.large}`,
-  borderBottom: `1px solid ${vars.color.border}`,
+  zIndex: 1000,
+  backgroundColor: vars.color.white,
+  padding: `${vars.spacing.large} ${vars.spacing['5xl']}`,
+  borderBottom: `2px solid ${vars.color.border}`,
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'space-between',
   width: '100%',
+  boxSizing: 'border-box',
 });
 
 export const logo = style({
